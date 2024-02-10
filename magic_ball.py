944ad57a7630d050bce94ed5f4b18d3f86a19e6b
Dannil <daniilyrkin@yandex.ru>
2024-02-10 15:17:26 +0300

    'Немного поменял код)'

--- magic_ball.py
+++ magic_ball.py
@@ -6,9 +6,19 @@ def start():
     print('Привет Мир, я магический шар, и я знаю ответ на любой твой вопрос.')
     user_name = str(input('Как тебя зовут? '))
     print(f'Приветствую тебя {user_name}\nНапиши вопрос который тебя волнует и я дам тебе ответ!')
+    answet_to_question()
+
+
+def answet_to_question():
     question = str(input('Введи вопрос: '))
     answe = random.choice(answers)
     print(f'Мой ответ:\n{answe}')
+    question = int(input('Будут ли еще вопросы? 1=Да 0=Нет\n'))
+    print(question)
+    if question == 1:
+        answet_to_question()
+    else:
+        print('Возвращайся если возникнут вопросы!')
 
 
 if __name__ == "__main__":
